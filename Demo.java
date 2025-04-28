89226f2e2ec6b9815a1b22244727543fbbc7a581
Rishi Sinha <rkrsinha07@gmail.com>
2025-04-28 17:32:11 +0530

    changes in demo class

--- Demo.java
+++ Demo.java
@@ -2,6 +2,7 @@
    public static void main (String [] args){
   System.out.print("Hello World");
   System.out.print("Working on Jenkins");
+  System.out.print("cHGECKING FOR pOLLscm");
 
      }
  }
